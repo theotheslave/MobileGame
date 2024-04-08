d2efb21caefb10181eb2cec7df261d89b8cd0733
menasthemenace <152167610+menasthemenace@users.noreply.github.com>
2024-04-08 22:45:35 +0200

    pick up UI text

---
+++ Mobile Game Project/Assets/Scripts/TextPickup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextPickup : MonoBehaviour
+{
+    public GameObject UiObject;
+    public GameObject CollectItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObject.SetActive(false);
+    }
+     void OnTriggerEnter(Collider other)
+    {
+       if(other.tag=="Player")
+        {
+            UiObject.SetActive(true);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        UiObject.SetActive(false);
+        Destroy(CollectItem);
+    }
+}
